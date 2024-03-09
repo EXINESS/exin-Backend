a1fa7e17aaaaf96386e86a90dbe336c29741ae83
sepide <infoexiness@gmail.com>
2024-03-09 23:40:33 +0330

    change dbcontext

--- backend/Infrastucture/exinDBContext.cs
+++ backend/Infrastucture/exinDBContext.cs
@@ -14,6 +14,6 @@ namespace backend.Infrastucture
             // connect to sql server database
             options.UseSqlServer(Configuration.GetConnectionString(""));
         }
-        //public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
