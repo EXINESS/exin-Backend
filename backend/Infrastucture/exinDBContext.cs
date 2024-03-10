bafe02cd853c5276a75bd34b325436c3b2398bec
sepide <sepidegholizad@gmail.com>
2024-03-11 00:24:25 +0330

    add database in lpcal

--- backend/Infrastucture/exinDBContext.cs
+++ backend/Infrastucture/exinDBContext.cs
@@ -12,7 +12,7 @@ namespace backend.Infrastucture
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             // connect to sql server database
-            options.UseSqlServer(Configuration.GetConnectionString(""));
+            options.UseSqlServer(Configuration.GetConnectionString("Server=(LocalDB)\\MSSQLLocalDB;Database=exinDB"));
         }
         //public DbSet<User> Users { get; set; }
     }
