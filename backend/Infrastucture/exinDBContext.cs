32b15cdb104bcac98b9930b0743f64e02ea866e0
sepide <sepidegholizad@gmail.com>
2024-03-14 02:06:38 +0330

    add connstr

--- backend/Infrastucture/exinDBContext.cs
+++ backend/Infrastucture/exinDBContext.cs
@@ -12,7 +12,7 @@ namespace backend.Infrastucture
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             // connect to sql server database
-            options.UseSqlServer(Configuration.GetConnectionString("Data Source=.;Initial Catalog=DB name;Integrated Security=True;MultipleActiveResultSets=True;"));
+            options.UseSqlServer(Configuration.GetConnectionString("Data Source=/C:/Users/PCSAM/Desktop/MyExin. db;Version=3"));
         }
         public DbSet<User> Users { get; set; }
     }
