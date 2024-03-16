19edc49a4dc08ba0628381babe0c884da4f66701
sepide <sepidegholizad@gmail.com>
2024-03-16 16:03:36 +0330

    add new

--- backend/Infrastucture/exinDBContext.cs
+++ backend/Infrastucture/exinDBContext.cs
@@ -12,7 +12,7 @@ namespace backend.Infrastucture
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             // connect to sql server database
-            options.UseSqlServer(Configuration.GetConnectionString("Data Source=/C:/Users/PCSAM/Desktop/MyExin. db;Version=3"));
+            options.UseSqlServer(Configuration.GetConnectionString("server=(LocalDB)\\MSSQLLocalDB;Database=exinDB.mdf"));
         }
         public DbSet<User> Users { get; set; }
     }
