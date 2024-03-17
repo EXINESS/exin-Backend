fe666dda405e25a1cbe7e563695d8a97594c86d3
sepide <sepidegholizad@gmail.com>
2024-03-17 11:17:56 +0330

    add startup class

---
+++ backend/Startup.cs.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+namespace backend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Other services...
+            services.AddRequestTimeouts();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Other middleware...
+            app.UseRequestTimeouts();
+        }
+
+    }
+}
