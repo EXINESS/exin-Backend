133478fb5ecc0d2a9700496fec1991bd0552904d
sepide <sepidegholizad@gmail.com>
2024-03-17 12:23:42 +0330

    add config file

---
+++ backend/Infrastucture/Config/UserConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using backend.Domain.Cores;
+
+namespace backend.Infrastucture.Config
+{
+    public class UserConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+            builder.Property(x=>x.Username).IsRequired();
+            builder.Property(x=>x.Password).IsRequired();
+            builder.Property(x=>x.N).IsRequired();
+        }
+    }
+}
