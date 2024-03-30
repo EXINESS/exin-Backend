14290462e1df0b6d034ee9b2db94da9b13c68440
sepide <sepidegholizad@gmail.com>
2024-03-30 23:43:25 +0330

    add new api

--- backend/Controllers/ReportController.cs
+++ backend/Controllers/ReportController.cs
@@ -6,6 +6,16 @@ using backend.Domain.Cores;
 using backend.Infrastucture;
 using backend.Services;
 using Microsoft.AspNetCore.Http.HttpResults;
+using AutoMapper;
+using backend.Domain.Cores.AchiveAggregate;
+using backend.Domain.Cores.TokenAggregate;
+using backend.Domain.Cores.TargetAggregate;
+using backend.Domain.Cores.SubTaskAggregate;
+using backend.Models.Targets;
+using backend.Models.SubTaskDtos;
+using backend.Models.FeedbackDtos;
+using backend.Models.AchiveDtos;
+using backend.Domain.Cores.FeedbackAggregate;
 
 namespace backend.Controllers
 {
@@ -13,5 +23,48 @@ namespace backend.Controllers
     [Route("[controller]/[action]")]
     public class ReportController:ControllerBase
     {
+        private readonly exinDBContext _dBContext;
+        private readonly IMapper _mapper;
+        private readonly IAchiveRepository _achiveRepository;
+        private readonly ITokenRepository _tokenRepository;
+        private readonly ISubTaskRepository _subTaskRepository;
+        private readonly ITargetRepository _targetRepository;
+        private readonly IFeedbackRepository _feedbackRepository;
+
+        public ReportController(exinDBContext exinDBContext,IMapper mapper,IAchiveRepository achiveRepository,ITokenRepository tokenRepository,ISubTaskRepository subTaskRepository,ITargetRepository targetRepository,IFeedbackRepository feedbackRepository)
+        {
+            exinDBContext = _dBContext;
+            mapper = _mapper;
+            achiveRepository = _achiveRepository;
+            tokenRepository = _tokenRepository;
+            subTaskRepository = _subTaskRepository;
+            targetRepository = _targetRepository;
+            feedbackRepository = _feedbackRepository;
+        }
+
+        [HttpGet("{achiveId}")]
+        public async Task<ActionResult<AchiveDto>> GetAchiveAsync(Guid achiveId,Token token)
+        {
+
+        }
+
+        [HttpGet("{targetId}")]
+        public async Task<ActionResult<TargetDto>> GetTargetAsync(Guid targetId, Token token)
+        {
+
+        }
+
+        [HttpGet("{SubtaskId}")]
+        public async Task<ActionResult<SubTaskDto>> GetSubtaskAsync(Guid SubtaskId, Token token)
+        {
+
+        }
+
+        [HttpGet("{feedbackId}")]
+        public async Task<ActionResult<FeedbackDto>> GetFeedbackAsync(Guid feedbackId, Token token)
+        {
+
+        }
+
     }
 }
---
+++ backend/Domain/Cores/FeedbackAggregate/IFeedbackRepository.cs
@@ -0,0 +1,9 @@
+﻿using backend.Domain.Cores.TokenAggregate;
+
+namespace backend.Domain.Cores.FeedbackAggregate
+{
+    public interface IFeedbackRepository
+    {
+        Task<Feedback?> GetFeedbackByIdAsync(Guid guid, Token token);
+    }
+}
