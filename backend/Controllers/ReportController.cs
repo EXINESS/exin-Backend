e35a29ab5a9d42d3db77122ed6e3dceeeaa99c7d
sepide <sepidegholizad@gmail.com>
2024-03-31 01:49:01 +0330

    add new class

--- backend/Controllers/ReportController.cs
+++ backend/Controllers/ReportController.cs
@@ -16,6 +16,9 @@ using backend.Models.SubTaskDtos;
 using backend.Models.FeedbackDtos;
 using backend.Models.AchiveDtos;
 using backend.Domain.Cores.FeedbackAggregate;
+using backend.Models.TokenDtos;
+using Microsoft.AspNetCore.Authorization;
+
 
 namespace backend.Controllers
 {
@@ -42,16 +45,79 @@ namespace backend.Controllers
             feedbackRepository = _feedbackRepository;
         }
 
+        [HttpGet("{token}")]
+        public async Task<ActionResult<TokenDto>> GetTokenAsync(Domain.Cores.TokenAggregate.Token token)
+        {
+            var _token = await _tokenRepository.GetTokenAsync(token);
+            if (_token is null)
+            {
+                return NotFound($"targetId{token}not found");
+            }
+            var tokenDto = _mapper.Map<TokenDto>(_token);
+            return Ok(tokenDto);
+        }
+        [Authorize]
+        public async Task<ActionResult<TokenDto>> CheckTokenAsync(Token token)
+        {
+            var item = await _tokenRepository.CheckTokenAsync(token);
+
+            if (item is null)
+            {
+                return NotFound($"UserId{token}notfound");
+            }
+            else if (item.Timeout > DateTime.Now.TimeOfDay)
+            {
+
+                _tokenRepository.DelTokenAsync(token);
+                return NotFound($"UserId{token}notfound");
+            }
+            else
+            {
+                var tokenmodel = _mapper.Map<Token>(item);
+                return Ok(tokenmodel);
+            }
+        }
+        public async Task<ActionResult<TokenDto>> DelTokenAsync(Domain.Cores.TokenAggregate.Token token)
+        {
+            var _token = await _tokenRepository.GetTokenAsync(token);
+            if (token is null)
+            {
+                return NotFound($"targetid{token}notfound");
+            }
+            await _tokenRepository.DelTokenAsync(_token);
+            return Ok();
+        }
+
         [HttpGet("{achiveId}")]
         public async Task<ActionResult<AchiveDto>> GetAchiveAsync(Guid achiveId,Token token)
         {
-
+            if (CheckTokenAsync(token) != null)
+            {
+                var achive = await _achiveRepository.GetAchiveByIdAsync(achiveId, token);
+                if (achive is null)
+                {
+                    return NotFound($"achiveId{achiveId}not found");
+                }
+                var achiveDto = _mapper.Map<AchiveDto>(achive);
+                return Ok(achiveDto);
+            }
+            return Unauthorized();
         }
 
         [HttpGet("{targetId}")]
         public async Task<ActionResult<TargetDto>> GetTargetAsync(Guid targetId, Token token)
         {
-
+            if (CheckTokenAsync(token) != null)
+            {
+                var target = await _targetRepository.GetTargetByIdAsync(targetId, token);
+                if (target is null)
+                {
+                    return NotFound($"targetId{targetId}not found");
+                }
+                var targetDto = _mapper.Map<TargetDto>(target);
+                return Ok(targetDto);
+            }
+            return Unauthorized();
         }
 
         [HttpGet("{SubtaskId}")]
